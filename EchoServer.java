b6ed0793cbe59ecddf19319b5a715ea4b144f13b
matteone03 <74899153+matteone03@users.noreply.github.com>
2023-08-30 11:52:31 +0200

    Update EchoServer.java

--- EchoServer.java
+++ EchoServer.java
@@ -1,27 +1,28 @@
 import java.net.*;
 import java.io.*;
 public class EchoServer extends Thread{
-	private DatagramSocket socket; // attributo che specifica l'indirizzo della socket
+    private DatagramSocket socket; // attributo che specifica l'indirizzo della socket
     private boolean running;	  // attributo che specifica lo stato del process
     private byte[] buf = new byte[256	];	//array di byte per unire i messaggi
 
     public EchoServer() throws SocketException{	// costruttore, stabilisce la porta di comunicazione
         socket = new DatagramSocket(4445);
     }
-    
-    public void run() {		// metodo che aspetta l'arrivo di un messaggio e lo rimanda indietro
-        running = true;		// alla variabile running viene assegnato true fino a quando non 
-        					// non arriva un errore o il messaggio "end" dal client
+
+    /*metodo che aspetta l'arrivo di un messaggio e lo rimanda indietro
+    alla variabile running viene assegnato true fino a quando non
+    non arriva un errore o il messaggio "end" dal client*/
+	
+    public void run() {
+        running = true; 
         while (running) {
         	try {
 	            DatagramPacket packet = new DatagramPacket(buf, buf.length); // variabile istanziata per ricevere i messaggi
 	            socket.receive(packet); //ricezione del messaggio
-	            
 	            InetAddress address = packet.getAddress(); 	// variabile che memorizza l'ip del client
 	            int port = packet.getPort(); 				// numero porta del client
 	            packet = new DatagramPacket(buf, buf.length, address, port);	//messaggio di risposta
 	            String received = new String(packet.getData(), 0, packet.getLength());	//Stringa uguale a quella ricevuta
-	            
 	            if (received.equals("end")) {
 	                running = false;
 	                continue;
